b197ee5d1f8d76b3a5c37a97d783a4c2e0ea1ebe
akhampariya <akhampariya@unomaha.edu>
2018-01-10 18:23:01 -0500

    update prod db

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 import os, dj_database_url
-from whitenoise import WhiteNoise
 from .localsettings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -128,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #if ENVIRONMENT == 'PROD':
 #    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #
@@ -136,9 +135,6 @@ USE_TZ = True
 #    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
 #    'PAGE_SIZE': 10
 #}
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
