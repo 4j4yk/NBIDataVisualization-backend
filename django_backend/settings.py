33b4f4e5b692e73ee563408142c4fde8ee8a6f37
akhampariya <akhampariya@unomaha.edu>
2018-01-10 18:34:43 -0500

    update prod db

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #if ENVIRONMENT == 'PROD':
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #
 #REST_FRAMEWORK = {
 #    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
